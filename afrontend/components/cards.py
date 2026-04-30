f5694a41ce5ff75b6fb7fa96d130c59ef3b77bd1
Neeraj Nagore <168879715+NeerajNagore@users.noreply.github.com>
2026-04-30 15:51:13 +0530

    Delete afrontend directory

--- afrontend/components/cards.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-def info_card(title, value):
-    st.metric(label=title, value=value)
--- afrontend/components/charts.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import matplotlib.pyplot as plt
-
-def show_bar_chart(labels, values):
-    fig, ax = plt.subplots()
-    ax.bar(labels, values)
-    st.pyplot(fig)
